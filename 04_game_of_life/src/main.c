861778c39169834859b40585c4238769ff493de6
Florian Hofstetter <flo.max.hofstetter@gmail.com>
2020-12-11 13:12:54 +0100

    Add simulation loop.

--- 04_game_of_life/src/main.c
+++ 04_game_of_life/src/main.c
@@ -4,33 +4,35 @@
 #include "followingCellState.h"
 #include "followingFieldState.h"
 #include <string.h>
+#include <unistd.h>
 
-#define MATCHFIELD 5
+#define MATCHFIELD 10
 #define MATCHFIELD_H MATCHFIELD
 #define MATCHFIELD_W MATCHFIELD
 
 int main()
 {
-    int cells[MATCHFIELD_H][MATCHFIELD] = {
-            {0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0},
-            {0, 1, 1, 1, 0},
-            {0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0},
+    int cellsActualState[MATCHFIELD_H][MATCHFIELD] = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
     };
-
-    int cellsCopy[MATCHFIELD_W][MATCHFIELD_H];
-    // memcpy(&cellsCopy, &cells, MATCHFIELD_W * MATCHFIELD_H * sizeof(int));
-
-    followingFieldState(MATCHFIELD_W, MATCHFIELD_H, &cells, &cellsCopy);
-
-    show_cells(MATCHFIELD_H, MATCHFIELD_W, cellsCopy);
-
-    // int neighbours[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-    // int actState = 0;
-    // actState = followingCellState(&actState, &neighbours);
-    // printf("State: %i", actState);
+    int cellsNextState[MATCHFIELD_W][MATCHFIELD_H];
 
 
+    for (int i=0; i<100; i++)
+    {
+        show_cells(MATCHFIELD_H, MATCHFIELD_W, cellsActualState);
+        followingFieldState(MATCHFIELD_W, MATCHFIELD_H, &cellsActualState, &cellsNextState);
+        memcpy(&cellsActualState, &cellsNextState, MATCHFIELD_W * MATCHFIELD_H * sizeof(int));
+        usleep(250*1e3); // ms
+    }
     return 0;
 }
